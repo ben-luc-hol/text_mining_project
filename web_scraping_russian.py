fe873aea86fa20a5a4811bea73601b0ffd86e4ca
ben-luc-hol <ben.luc.holden@gmail.com>
2023-03-06 15:20:04 -0700

    translation to english in progress

--- web_scraping_russian.py
+++ web_scraping_russian.py
@@ -1,16 +1,43 @@
+
+#*******************************************************************************************************************************************
+
+#*****************                   RUSSIAN LANGUAGE MEDIA COVERAGE OF THE WAR IN UKRAINE            **************************************
+
+#*******************************************************************************************************************************************
+
+# Import required libraries for scraping and formatting data:
+
 import pandas as pd
 import requests
 import pickle
 import time
 from bs4 import BeautifulSoup
 
+#*******************************************************************************************************************************************
+
+#*****************                   LOCATING KOMSOMOLSKAYA PRAVDA TEXT DATA                            ************************************
+
+#*******************************************************************************************************************************************
+
+
+
 #### Scraping Komsomolskaya Pravda's "Special Military Operation" topic page
 
-## ## Through inspecting the Network dev tool in Chrome browser,
-# API endpoint was identified, which should be easier than scraping the raw HTML.
+# Topic URL found here: https://www.kp.ru/daily/euromaidan/
 
-#Test most recent article page (762) and verifying the other params lead to the
-# special operations page. Check the output:
+
+# Used browser inspect and network dev tool to familiarize with the website/html structure.
+# Website contains "load more" button.
+# Using network tool, the website was found to be structured in pages containing article links,
+# where each 'load more' click calls an API to load another page of articles.
+
+# API endpoint was identified, structure was studied.
+# This is more convenient than attempting to simulate 'load more' clicks and scraping the html directly.
+
+# As of March 5, the most recent article page = 762.
+# API params were verified to lead to the correct topic page.
+
+## Testing API call on most recent article directory page:
 
 api_url = 'https://s02.api.yc.kpcdn.net/content/api/1/pages/get.json'
 params = {
@@ -26,15 +53,17 @@ response762 = requests.get(api_url,params)
 json_response762 = response762.json()
 print(json_response762)
 
-## Examining the JSON output and dictionary structure, it becomes clear
-# That class 13 denotes unique articles. It also becomes clear that the
-#@id within class 13 is a unique identifier for articles, and that it
-#leads to the article in question when pasted into the following link:
+## This was successful.
+
+## When examining the JSON output and dictionary structure, it becomes clear that 'class 13' in the dictionary contains
+# information on unique articles.
+
+# '@id' within class 13 is identified as a unique identifier for articles.
+# This article ID number can evidently be pasted directly into the following link, which leads directly to a given article:
 
 # https://www.kp.ru/daily/27473.5/{@id}/
 
-## First, test out extracting these @id values from the dictionary
-# from the most recent page requested:
+## First, test out extracting these @id values from the dictionary (using the most recent articles page):
 test_kp_list = []
 for child in json_response762['childs']:
     for grandchild in child['childs']:
@@ -42,18 +71,24 @@ for child in json_response762['childs']:
             article_id = grandchild['@id']
             test_kp_list.append(article_id)
 
+# This successfully produces a list of unique article IDs!!
 print(test_kp_list)
 
 
-## NOW: Get ARTICLE IDs for all of Komsomolskaya's coverage from late-November to present:
+##  *****   Get ARTICLE IDs for all of KP's war coverage from late-November to present:
 
 #Empty list for article ID numbers:
 kp_article_ids = []
+
 # Using network dev tool in Chrome, the unique API endpoint for pages under the "special operation"
 # topic page in komsomolskaya's coverage was identified.
+
 #The range of articles to be scraped is from 710 (~November 28) to 762 (current).
+# Loop through the articles pages to scrape all class 13 article IDs:
 for page_no in range(710, 762):
     api_url = 'https://s02.api.yc.kpcdn.net/content/api/1/pages/get.json'
+
+    ## Correct API parameters were identified with Chrome inspect tools
     params = {
          'pages.direction':'page',
          'pages.number': page_no,
@@ -65,38 +100,62 @@ for page_no in range(710, 762):
     json_response = response.json()
 
     #The JSON file is structured such that 'class 13' denotes articles.
+    #Extract the article IDs only and append to empty list:
     for child in json_response['childs']:
         for grandchild in child['childs']:
             if grandchild['@class'] == 13:
                 article_id = grandchild['@id']
                 kp_article_ids.append(article_id)
 
-## Pickle article list to avoid having to re-run above code:
+
+## Save article IDs list as a pickle file to avoid having to re-run above code - Just in case:
 
 with open('data/kp_article_id.pkl', 'wb') as f:
     pickle.dump(kp_article_ids, f)
 
-#Reload pickle file
+
+#*******************************************************************************************************************************************
+
+#*****************                   SCRAPING & SAVING KOMSOMOLSKAYA PRAVDA ARTICLES                            ****************************
+
+#*******************************************************************************************************************************************
+
+
+#Reload pickle file with list of unique KP articles:
+
 with open('data/kp_article_id.pkl', 'rb') as f:
     kp_article_ids = pickle.load(f)
 
-print(kp_article_ids)
+# Ensure the list was loaded right
+#print(kp_article_ids)
 
 
 # Now, test web scraping for each article's title, date, and content:
 
-#Print the first article to use for scraping purposes -- check link in browser
+#Print the first article to use for scraping purposes --  checking link in browser
 #print(f'https://www.kp.ru/daily/27473.5/{kp_article_ids[0]}/')
 #print(f'https://www.kp.ru/daily/27473.5/{kp_article_ids[1]}/')
 #print(f'https://www.kp.ru/daily/27473.5/{kp_article_ids[2]}/')
 
 
+# Testing the first URL to article:
 test_scrape_url = requests.get(f'https://www.kp.ru/daily/27473.5/{kp_article_ids[0]}')
 
-# Dump url into BeautifulSoup:
+# Using bs4 to parse the article's html structure:
 test_soup = BeautifulSoup(test_scrape_url.text, 'html5lib')
 
-#Write functions to extract key elements:
+
+# By inspecting the website in the browser, the specific elements (classes) to be scraped
+# were identified. Functions below extract these core elements:
+
+# * Headline of article
+# * Subheading of article
+# * Author of article
+# * Date and time published
+# * Text content/ body of article
+
+
+#Writing functions to extract key elements:
 
 #Headline
 def get_headline(soup_object):
@@ -130,47 +189,58 @@ def get_date(soup_object):
 #print(get_date(test_soup))
 
 
-#Test on random article:
+#Testing functions on random article:
 
 test2 = requests.get(f'https://www.kp.ru/daily/27473.5/{kp_article_ids[500]}/')
+print(f'Status: {test2.status_code}\n')
 test2_text = test2.text
-
 test_soup2 = BeautifulSoup(test2_text, 'html5lib')
 
-print(get_headline(test_soup2))
-print(get_subheading(test_soup2))
-print(get_date(test_soup2))
-print(get_author(test_soup2))
-print(get_content(test_soup2))
+print(f'Test Headline: {get_headline(test_soup2)} \n')
+print(f'Test Subheading: {get_subheading(test_soup2)} \n')
+print(f'Test Author: {get_author(test_soup2)} \n')
+print(f'Test Date: {get_date(test_soup2)} \n')
+print(f'Test Content: {get_content(test_soup2)}')
 
-#test2.status_code
+
+## Functions were successful!
+
+
+
+
 ########### GET ARTICLES IN FOR LOOP, STORE ARTICLES
 
-### LISTS
 
-#print(kp_article_ids)
-
+# Caffeinate computer (mac OS) if necessary:
 #subprocess.run(['caffeinate'])
 
+
+## Empty lists to append headline, subheading, author, date, and content elements scraped from kp.ru HTML:
 headlines = []
 subheadings = []
 authors = []
 dates = []
 contents = []
 
-for i, article in enumerate(kp_article_ids[617:]):
+## Loop through the article IDs scraped in the above code
+for i, article in enumerate(kp_article_ids):
 
-
+    # Paste article ID into the "special military operation" topic link
     url = f'https://www.kp.ru/daily/27473.5/{article}/'
+
+    # Request article endpoint html
     response = requests.get(url)
 
+    # Run script if connection successful
     if response.status_code == 200:
 
         print(f'# {article} Response Successful. \n')
         print(f'Scraping article {i+1}/{len(kp_article_ids)} with ID {article}\n')
 
+        # bs4 object to parse html
         soup = BeautifulSoup(response.text, 'html5lib')
 
+        # Run pre-defined functions to get article elements, return None if an exception is raised
         try:
             headline = get_headline(soup)
         except:
@@ -200,10 +270,13 @@ for i, article in enumerate(kp_article_ids[617:]):
         except:
             print("Content not found")
             content = None
+    # If connection unsuccessful, skip to next article
     else:
         continue
         print(f'{article} not scraped. Skipping.\n')
 
+    # Append successfully scraped elements to corresponding empty lists; append 'None' if an exception was raised for the field,
+    # This ensures that the lists are of same length and with each appended element corresponding to those in other lists by index
     if all(x is not None for x in [headline, subheading, date, author, content]):
         headlines.append(headline)
         subheadings.append(subheading)
@@ -220,19 +293,22 @@ for i, article in enumerate(kp_article_ids[617:]):
         print(f"{article} not fully scraped. \n")
 
     print(f'#{article} Complete.\n')
+
+   # Consider spacing out scrapes by x seconds to avoid excessive server requests, if necessary:
    # time.sleep(3)
 
 #%%
 
+# Ensure the lists are of same size:
 #print(len(headlines))
 #print(len(subheadings))
 #print(len(authors))
 #print(len(dates))
 #print(len(contents))
 
-#print(kp_article_ids[616:])
 
-### Save dataframe
+
+### SAVE RAW DATAFRAME:
 
 data = pd.DataFrame({
     'source':'KomsomolskayaPravda',
@@ -246,25 +322,40 @@ data = pd.DataFrame({
 })
 
 data.to_csv('data/komsomolskaya_pravda_raw.csv', index=False)
-data.to_parquet('data/kp_raw.parquet')
+#data.to_parquet('data/kp_raw.parquet')
 
-## Remove data with none types
+
+#*******************************************************************************************************************************************
+
+#*****************                   TRANSLATE KOMSOMOLSKAYA PRAVDA TEXT DATA                         **************************************
+
+#*******************************************************************************************************************************************
+
+from creds.apikeys import translatorKey1
+import uuid
+import json
+
+
 data = pd.read_csv('data/komsomolskaya_pravda_raw.csv')
 
+## REMOVE ARTICLES CONTAINING 'NONE'
 data = data[data['article_content'].notnull()].copy()
 
 
 
-
+## Strip some strings from the content
 data['article_content'] = data['article_content'].str.strip('[').str.strip(']')
 data['article_content'] = data['article_content'].str.strip("'")
 data['article_content'] = data['article_content'].str.replace(".', '", ". ")
+
+# Print 1 content observation to ensure that it worked
+# Russian language does not use apostrophes, stripping them will not interfere with translation
+# since any apostrophes will have emerged from data formatting.
+
 print(data.iloc[0,7])
-data
 
 
-data
-## Translate dataframe
+## 3 Columns to translate. Determine the number of characters contained in each"
 len_head = sum(len(text) for text in data["headline"])
 len_sub = sum(len(text) for text in data["subheadings"])
 len_cont = sum(len(text) for text in data["article_content"])
@@ -276,20 +367,14 @@ print(f'Total characters to translate: {sum([len_cont,len_sub,len_head])}')
 
 
 ## Microsoft Azure will be used to translate the text data from Russian into English.
-## Roughly ~3.5 million characters will be translated. 2 million on free tier, the rest
-## Using student credits.
 
-## Test translation on headlines:
+##  ~3.5 million characters will be translated. 2 million on free tier, the rest using academic use credits.
 
-from creds.apikeys import translatorKey1
-import uuid
-import json
-
+## Initialize Translation API
 endpoint = 'https://api.cognitive.microsofttranslator.com/'
 path = '/translate'
 location = 'eastus'
 key = translatorKey1
-
 url = endpoint+path
 
 headers = {
@@ -299,14 +384,124 @@ headers = {
     'X-ClientTraceId': str(uuid.uuid4())
 }
 
-params = {
-    'api-version':'3.0',
-    'from':'ru'
-    'to':'en'
 
+# Run translation on the headline column (73192 characters):
+
+### Testing the translation code on a single headline:
+
+test_headline = data.loc[12, 'headline']
+print(f'Russian headline: {test_headline}\n')
+
+params = {
+    'api-version': '3.0',
+    'from': 'ru',
+    'to': 'en'
 }
 
+body = [{
+    'text': test_headline
+}]
 
+r_test = requests.post(url, params=params, headers=headers, json=body)
+re_test = r_test.json()
+
+print(f'Dictionary structure of translation:\n {re_test}\n')
+
+print(f'Translated headline: {re_test[0]["translations"][0]["text"]}\n')
+
+
+
+
+### Create emptu translated dataset:
+kp_data_english = data
+#kp_data_english
+
+
+#### Translate the headline column:
+
+# Empty list of translated headlines:
+
+translated_headlines = []
+
+for i, row in data.iterrows():
+
+    # Specify original headline:
+    russian_headline = row['headline']
+
+    body = [{
+        'text': russian_headline
+    }]
+    # Set params
+    params = {
+        'api-version': '3.0',
+        'from': 'ru',
+        'to': 'en'
+    }
+    # Post request to translation API
+    r = requests.post(url, params=params, headers=headers, json=body)
+    r_json = r.json()
+
+    # Extract the translated text from the API response and store it in the translated_data DataFrame
+    english_headline = r_json[0]['translations'][0]['text']
+    translated_headlines.append(english_headline)
+
+
+
+print(translated_headlines)
+
+kp_data_english['headline'] = translated_headlines
+
+
+
+
+### TRANSLATE SUBHEADINGS:
+
+translated_subheadings = []
+
+for i, row in data.iterrows():
+
+    print(f'Requesting translation {i+1}/{len(data)} to API. \n')
+    # Specify original headline:
+    russian_subheading = row['subheadings']
+
+    body = [{
+        'text': russian_subheading
+    }]
+    # Set params
+    params = {
+        'api-version': '3.0',
+        'from': 'ru',
+        'to': 'en'
+    }
+    # Post request to translation API
+    r = requests.post(url, params=params, headers=headers, json=body)
+    r_json = r.json()
+
+    # Extract the translated text from the API response and store it in the translated_data DataFrame
+    english_subheading = r_json[0]['translations'][0]['text']
+    translated_subheadings.append(english_subheading)
+
+    # Progress report
+    print(f'Translation {i+1}/{len(data)} successful.\n')
+
+
+kp_data_english['subheadings'] = translated_subheadings
+kp_data_english
+
+
+
+
+
+
+
+### The API is likely to throw an error when hitting overage (2+ million characters).
+## Before translating the content, 
+
+
+
+
+# Save the translated data to a file
+translated_data.to_csv("translated_data_file.csv", index=False)
 
 
 
