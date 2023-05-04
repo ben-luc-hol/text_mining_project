70c9e9e134b7aa2226d3424e160e676bf21065da
ben-luc-hol <ben.luc.holden@gmail.com>
2023-05-04 01:52:57 -0600

    datasets created

--- data_cleaning.py
+++ data_cleaning.py
@@ -198,12 +198,163 @@ preprocessed_data['headline_summary'] = preprocessed_data['headline_summary'].ap
 preprocessed_data.to_csv('data/processed_data/preprocessed_data.csv/')
 
 
-## 6 datasets with count vectorizer:
+# Count vectorizer:
 
-vectorizer = CountVectorizer(input=)
+vectorizer100 = CountVectorizer(input='content',
+                                stop_words='english',
+                                max_features=100)
 
-dataframes = [preprocessed_data, stemmed_data, lemmed_data]
 
+headlines_01 = preprocessed_data['headline_summary']
+headlines_01
+
+CV_headlines_1 = vectorizer100.fit_transform(headlines_01)
+colnames = vectorizer100.get_feature_names()
+CV100_headlines_full = pd.DataFrame(CV_headlines_1.toarray(), columns = colnames)
+CV100_headlines_full.insert(0,'LABEL', source_label)
+CV100_headlines_full.to_csv('data/processed_data/headlines_unaltered_CV_100.csv', index=False)
+
+
+headlines_02 = stemmed_data['headline_summary']
+
+CV_headlines_2 = vectorizer100.fit_transform(headlines_02)
+colnames = vectorizer100.get_feature_names()
+colnames
+CV100_headlines_stemmed = pd.DataFrame(CV_headlines_2.toarray(), columns = colnames)
+CV100_headlines_stemmed.insert(0,'LABEL', source_label)
+
+CV100_headlines_stemmed.to_csv('data/processed_data/headlines_stemmed_CV_100.csv', index=False)
+
+
+
+headlines_03 = lemmed_data['headline_summary']
+
+CV_headlines_3 = vectorizer100.fit_transform(headlines_03)
+colnames = vectorizer100.get_feature_names()
+colnames
+CV100_headlines_lemmed = pd.DataFrame(CV_headlines_3.toarray(), columns = colnames)
+CV100_headlines_lemmed.insert(0,'LABEL', source_label)
+
+CV100_headlines_lemmed
+CV100_headlines_stemmed.to_csv('data/processed_data/headlines_lemmed_CV_100.csv', index=False)
+
+
+### CONTENT (Top 250 features)
+
+vectorizer250 = CountVectorizer(input='content',
+                                stop_words='english',
+                                max_features=250)
+
+
+
+content_01 = preprocessed_data['content']
+content_01
+
+CV_content_1 = vectorizer250.fit_transform(content_01)
+colnames = vectorizer250.get_feature_names()
+CV250_content_full = pd.DataFrame(CV_content_1.toarray(), columns = colnames)
+CV250_content_full.insert(0,'LABEL', source_label)
+CV250_content_full
+
+CV250_content_full.to_csv('data/processed_data/content_unaltered_CV_250.csv', index=False)
+
+
+content_02 = stemmed_data['content']
+content_02
+
+CV_content_2 = vectorizer250.fit_transform(content_02)
+colnames = vectorizer250.get_feature_names()
+CV250_content_stemmed = pd.DataFrame(CV_content_2.toarray(), columns = colnames)
+CV250_content_stemmed.insert(0,'LABEL', source_label)
+CV250_content_stemmed
+
+CV250_content_stemmed.to_csv('data/processed_data/content_stemmed_CV_250.csv', index=False)
+
+
+
+content_03 = lemmed_data['content']
+
+CV_content_3 = vectorizer250.fit_transform(content_03)
+colnames = vectorizer250.get_feature_names()
+CV250_content_lemmed = pd.DataFrame(CV_content_3.toarray(), columns = colnames)
+CV250_content_lemmed.insert(0,'LABEL', source_label)
+CV250_content_lemmed
+
+CV250_content_lemmed.to_csv('data/processed_data/content_lemmed_CV_250.csv', index=False)
+
+
+## Ngrams:
+
+content_01 = preprocessed_data['content']
+
+n_vectorizer12000 = CountVectorizer(input='content',
+                                    stop_words='english',
+                                    max_features=12000,
+                                    ngram_range=(2,4))
+
+
+CV_content_4 = n_vectorizer12000.fit_transform(content_01)
+colnames= n_vectorizer12000.get_feature_names()
+CV12000_content_orig = pd.DataFrame(CV_content_4.toarray(), columns = colnames)
+CV12000_content_orig.insert(0,'LABEL', source_label)
+
+CV12000_content_orig.to_csv('data/processed_data/CV_ngrams_12000.csv', index = False)
+
+
+n_vectorizer1000 = CountVectorizer(input='content',
+                                    stop_words='english',
+                                    max_features=1000,
+                                    ngram_range=(5,5))
+
+#New ngram dataframe:
+content_01 = preprocessed_data['content']
+
+n_vectorizer1000 = CountVectorizer(input='content',
+                                    stop_words='english',
+                                    max_features=1000,
+                                    ngram_range=(4,4))
+
+
+CV_content_5 = n_vectorizer1000.fit_transform(content_01)
+colnames= n_vectorizer1000.get_feature_names()
+CV1000_content_orig = pd.DataFrame(CV_content_5.toarray(), columns = colnames)
+CV1000_content_orig.insert(0,'LABEL', source_label)
+
+CV1000_content_orig
+
+CV1000_content_orig.to_csv('data/processed_data/CV_ngrams_1000.csv', index = False)
+
+
+#### TDIF
+
+
+tfid1000 = TfidfVectorizer(input= 'content',
+                           stop_words='english',
+                           max_features=1000)
+
+
+TF1000_content_orig = tfid1000.fit_transform(content_01)
+colnames = tfid1000.get_feature_names()
+TF1000_content_orig = pd.DataFrame(TF1000_content_orig.toarray(), columns=colnames)
+TF1000_content_orig.insert(0,'LABEL', source_label)
+
+TF1000_content_orig.to_csv('data/processed_data/TF_original_1000.csv')
+
+
+
+
+n_tfid1000 = TfidfVectorizer(input= 'content',
+                           stop_words='english',
+                           max_features=1000,
+                            ngram_range=(4,4))
+
+
+TF1000_content_orig_ngram = n_tfid1000.fit_transform(content_01)
+colnames = n_tfid1000.get_feature_names()
+TF1000_ngram = pd.DataFrame(TF1000_content_orig_ngram.toarray(), columns=colnames)
+TF1000_ngram.insert(0,'LABEL', source_label)
+
+TF1000_ngram.to_csv('data/processed_data/TF_ngrams_1000.csv', index = False)
 
 
 
